6967503841ff2d5e44392f657e1bcee08b3efc40
Tulasi Joshua <mbawinitulasi12@gmail.com>
2023-12-25 07:40:19 -0800

    tab navigation

---
+++ App.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CourseListScreen from './screens/CourseListScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import Ionicons from "@expo/vector-icons/Ionicons"
+
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarLabelPosition: "below-icon",
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "purple"
+      }}>
+        <Tab.Screen name="Course List" component={CourseListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarLabel: "My Profile",
+          tabBarIcon: ({ color }) => ( <Ionicons name="person" size={20} color={color} />),
+          tabBarBadge: 3
+        }} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
